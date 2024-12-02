2e1d1ea93d9fd184347d41294a1c55c4cb6722d4
Egorych1988 <egorych1988@mail.ru>
2024-12-03 00:08:57 +0300

    Update README.md

---
+++ task_1.py
@@ -0,0 +1,30 @@
+def check_round(num):
+    return int(num * 100) / 100 == num
+
+
+def calc_prices(input_price_with_nds, proc_nds):
+    coefficient = 1 + proc_nds / 100.0
+    up_price_with_nds = down_price_with_nds = int(input_price_with_nds * 100) / 100
+    up_price_without_nds = down_price_without_nds = up_price_with_nds / coefficient
+
+    while not check_round(up_price_without_nds) and not check_round(down_price_without_nds):
+        if down_price_with_nds > 0.01:
+            down_price_with_nds = round(down_price_with_nds - 0.01, 2)
+            down_price_without_nds = down_price_with_nds / coefficient
+        up_price_with_nds = round(up_price_with_nds + 0.01, 2)
+        up_price_without_nds = up_price_with_nds / coefficient
+
+    if check_round(up_price_without_nds):
+        corrected_price_with_nds = up_price_with_nds
+        corrected_price_without_nds = up_price_without_nds
+    else:
+        corrected_price_with_nds = down_price_with_nds
+        corrected_price_without_nds = down_price_without_nds
+
+    return round(corrected_price_with_nds, 2), round(corrected_price_without_nds, 2)
+
+
+# Пример использования
+if __name__ == "__main__":
+    prices_1 = calc_prices(1.81, 20)
+    print(f"CorrectedPriceWithNDS: {prices_1[0]}, CorrectedPriceWithoutNDS: {prices_1[1]}")
---
+++ task_2.py
@@ -0,0 +1,49 @@
+from task_1 import calc_prices, check_round
+import tkinter as tk
+from tkinter import messagebox
+
+
+def is_number(num):
+    return str(num).replace(".", "", 1).isdigit()
+
+
+# Функция для проверки корректности решения
+def check_solution():
+    input_price_with_nds = price_with_nds.get()
+    input_proc_nds = proc_nds.get()
+
+    if not is_number(input_price_with_nds):
+        result = 'Некорректный ввод. \nРекомендованная цена с НДС должна быть указана в виде целого или ' \
+                 'вещественного числа. \nПожалуйста, введите корректное цифровое значение.'
+    elif not is_number(input_proc_nds) or not 0 < float(input_proc_nds) < 99:
+        result = 'Некорректный ввод. \nПроцент НДС должен быть указан в виде целого или вещественного числа ' \
+                 'в интервале от 0 до 99. \nПожалуйста, введите корректное цифровое значение.'
+    else:
+        with_nds, without_nds = calc_prices(float(input_price_with_nds), float(input_proc_nds))
+        result = f"Цена c НДС = {with_nds}\nЦена без НДС = {without_nds}\nСтавка НДС = {input_proc_nds}"
+
+    # Показываем результат в messagebox
+    messagebox.showinfo("Результат проверки", result)
+
+
+# Создание основного окна
+root = tk.Tk()
+root.title("Проверка решения")
+
+# Создание метки и текстового поля для ввода
+price_with_nds = tk.Label(root, text="Рекомендованная цена с НДС")
+price_with_nds.pack(pady=3)
+price_with_nds = tk.Entry(root)
+price_with_nds.pack(pady=2)
+
+proc_nds = tk.Label(root, text="Ставка НДС")
+proc_nds.pack(pady=3)
+proc_nds = tk.Entry(root)
+proc_nds.pack(pady=2)
+
+# Создание кнопки для проверки
+check_button = tk.Button(root, text="Вычислить", command=check_solution)
+check_button.pack(pady=10)
+
+# Запуск основного цикла
+root.mainloop()
